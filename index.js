5d9a98586d7ce4a565acd57d92cfead391a54b1f
Dmytro Lobanov <dspwww@gmail.com>
2025-01-01 21:12:56 +0000

    3.11 task DONE

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ morgan.token('postBody', function (req, res) {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postBody'));
 
 let persons = [
